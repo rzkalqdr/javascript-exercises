17d7b55ca6b9491a72b9244c1fabcedfbf7fbda8
rizki <rzkalqdr@gmail.com>
2022-08-07 11:32:58 +0700

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
 const sumAll = function() {
+    if (arguments[0] < 0 || arguments[1] < 0 || (typeof arguments[0] !== 'number') || (typeof arguments[1] !== 'number')) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    let max = (arguments[0] > arguments[1]) ? arguments[0] : arguments[1];
+    let min = (arguments[0] < arguments[1]) ? arguments[0] : arguments[1];
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
