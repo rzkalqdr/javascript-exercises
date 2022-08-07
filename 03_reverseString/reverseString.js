0fa8053500eab1495c739aa31baeb2fb4b375f20
rizki <rzkalqdr@gmail.com>
2022-08-07 09:23:21 +0700

    finish 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let split = text.split("")
+    let reversed = '';
+    for (let i = split.length-1; i >= 0; i--) {
+        reversed += split[i];
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
