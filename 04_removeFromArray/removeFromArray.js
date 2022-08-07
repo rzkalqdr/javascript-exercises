1512cb7676677c178f135eb5237604e883f7aa56
rizki <rzkalqdr@gmail.com>
2022-08-07 11:22:41 +0700

    finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
 const removeFromArray = function() {
-
+    const args = Array.prototype.slice.call(arguments);
+    if (args.length >= 2) {
+        let array = args[0];
+        for (let i = 1; i < args.length; i++) {
+            let numIndex = array.indexOf(args[i]);
+            if (numIndex === -1) continue;
+            array.splice(numIndex, 1);
+        }
+        return array;
+    }
 };
 
 // Do not edit below this line
