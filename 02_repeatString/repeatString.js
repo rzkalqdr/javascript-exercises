36d7aace69efbc88b1358603b5f049e0f1e87947
rizki <rzkalqdr@gmail.com>
2022-08-07 09:08:22 +0700

    finish 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeat) {
+    if (repeat < 0) {
+        return 'ERROR';
+    }
+    let repeatedText = '';
+    for (let i = 0; i < repeat; i++) {
+        repeatedText += text;
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
