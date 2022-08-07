b409e076d389eb893d4eba0aa9b5b9bb8f8b36cb
rizki <rzkalqdr@gmail.com>
2022-08-07 12:46:32 +0700

    aksjdkasjd

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const ONLY_LETTERS = /[^a-z]/gi
 
+const palindromes = function (text) {
+    let equal = true;
+    let cleaned = text.toLowerCase().replace(ONLY_LETTERS, '').split('')
+
+    forward: for (let i = 0; i < cleaned.length; i++) {
+        for (let j = cleaned.length; j > i; j--) {
+            equal = cleaned[i] === cleaned[j];
+            if (!equal) break forward;
+        }
+    }
+
+    return equal;
 };
 
 // Do not edit below this line
