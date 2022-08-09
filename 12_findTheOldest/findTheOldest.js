691e7e352a6d2131123e9b197cd089b4631b8828
rizki <rzkalqdr@gmail.com>
2022-08-09 13:29:44 +0700

    finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    
+    for (person of people) {
+        Object.assign(person, {age: calculateAge(person)});
+        if (person['age'] >= oldest['age']) {
+            oldest = person;
+        }
+    }
+    
+    return oldest;
 };
+
+
+const calculateAge = function(person) {
+    if (person['yearOfDeath'] === undefined || person['yearOfDeath'] === null) {
+        const thisYear = new Date().getFullYear();
+        return thisYear - person['yearOfBirth'];
+    } else {
+        return person['yearOfDeath'] - person['yearOfBirth'];
+    }
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
